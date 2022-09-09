8a957dfda151c6b3b859659653f10124a749a494
Naihua Chen <naihua.chen@mail.goldennet.com.tw>
2022-09-09 16:41:15 +0800

    [Update--Added 專案上傳]

---
+++ README.md
@@ -0,0 +1,12 @@
+# React 18 初始環境範本
+* 框架語言：React 18
+* UI 框架：Bootstrap 5
+
+## Package
+* react-router-dom@5.2.1
+* node-sass@7.0.1
+* bootstrap@5.1.3
+* axios@0.27.2
+* @fortawesome/fontawesome-svg-core@6.1.1",
+* @fortawesome/free-solid-svg-icons@6.1.1",
+* @fortawesome/react-fontawesome@0.1.18",
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+//---stylesheet
+import './assets/stylesheet/all.scss';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+import AppRoute from './router/AppRoute';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AppRoute />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
